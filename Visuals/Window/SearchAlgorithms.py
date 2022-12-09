f0d7ac9ffa557aa48a876a3274153b7140a519f3
pheile <pheile@iu.edu>
2022-12-09 13:10:56 -0500

    classmethod

--- Visuals/Window/SearchAlgorithms.py
+++ Visuals/Window/SearchAlgorithms.py
@@ -12,101 +12,104 @@
 #--------------------------------
 import math
 from queue import PriorityQueue
+class Algorithms:
 
-def heuristic1(point_s,point_g):
-    # a basic heuristic that uses euclidean distance, returns a float
-    return math.sqrt((point_s-point_g)**2 + (point_s-point_g)**2)
-#--------------------------------
-def heuristic2(a,b):
-    # Manhattan distance on a square grid
-    return abs(a.x - b.x) + abs(a.y - b.y)
+    def heuristic1(point_s,point_g):
+        # a basic heuristic that uses euclidean distance, returns a float
+        return math.sqrt((point_s-point_g)**2 + (point_s-point_g)**2)
+    #--------------------------------
 
-#--------------------------------
-def AStarSearch(point, destination1, destination2):
-    frontier = PriorityQueue()
-    x = point.x
-    y = point.y
-    start = (x,y)
-    frontier.put(start, 0)
-    came_from = dict()
-    cost_so_far = dict()
-    came_from[start] = None
-    cost_so_far[start] = 0
-    goal = destination1
+    def heuristic2(a,b):
+        # Manhattan distance on a square grid
+        return abs(a.x - b.x) + abs(a.y - b.y)
 
-    while not frontier.empty():
-        current = frontier.get()
+    #--------------------------------
+    @classmethod
+    def AStarSearch(point, destination1, destination2):
+        frontier = PriorityQueue()
+        x = point.x
+        y = point.y
+        start = (x,y)
+        frontier.put(start, 0)
+        came_from = dict()
+        cost_so_far = dict()
+        came_from[start] = None
+        cost_so_far[start] = 0
+        goal = destination1
 
-        if current == goal:
-            break
-    
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic1(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == goal:
+                break
         
-    
+            for next in graph.neighbors(current):
+                new_cost = cost_so_far[current] + graph.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + heuristic1(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+            
 
-#--------------------------------
-#--------------------------------
-def greedySearch(point, destination1, destination2):
-    x = point.x
-    y = point.y
-    start = (x,y)
-    goal = destination1
-    
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = dict()
-    came_from[start] = None
-
-    while not frontier.empty():
-        current = frontier.get()
-
-        if current == goal:
-            break
+    #--------------------------------
+    #--------------------------------
+    @classmethod
+    def greedySearch(point, destination1, destination2):
+        x = point.x
+        y = point.y
+        start = (x,y)
+        goal = destination1
         
-        for next in graph.neighbors(current):
-            if next not in came_from:
-                priority = heuristic1(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = dict()
+        came_from[start] = None
 
+        while not frontier.empty():
+            current = frontier.get()
 
-#--------------------------------
-#--------------------------------
-def MarkovSearch(point):
-    pass
+            if current == goal:
+                break
+            
+            for next in graph.neighbors(current):
+                if next not in came_from:
+                    priority = heuristic1(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
 
+    #--------------------------------
+    #--------------------------------
+    @classmethod
+    def MarkovSearch(point):
+        pass
 
-#--------------------------------
-#--------------------------------
-def DijkstrasSearch(point, destination1, destination2):
-    x = point.x
-    y = point.y
-    start = (x,y)
-    goal = destination1
-    
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = dict()
-    cost_so_far = dict()
-    came_from[start] = None
-    cost_so_far[start] = 0
-
-    while not frontier.empty():
-        current = frontier.get()
+    #--------------------------------
+    #--------------------------------
+    @classmethod
+    def DijkstrasSearch(point, destination1, destination2):
+        x = point.x
+        y = point.y
+        start = (x,y)
+        goal = destination1
         
-        if current == goal:
-            break
-        
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost
-                frontier.put(next, priority)
-                came_from[next] = current
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = dict()
+        cost_so_far = dict()
+        came_from[start] = None
+        cost_so_far[start] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+            
+            if current == goal:
+                break
+            
+            for next in graph.neighbors(current):
+                new_cost = cost_so_far[current] + graph.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost
+                    frontier.put(next, priority)
+                    came_from[next] = current
